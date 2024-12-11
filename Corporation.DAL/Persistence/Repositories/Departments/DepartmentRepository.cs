7a049195077c3687241679136e4eccc0cb8dd094
Mamhoud Adel <mahmoudadel2199@gamil.com>
2024-12-11 18:01:22 +0200

    Department Repository

---
+++ Corporation.DAL/Persistence/Repositories/Departments/DepartmentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Corporation.DAL.Models.Department;
+using Corporation.DAL.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporation.DAL.Persistence.Repositories.Departments
+{
+    internal class DepartmentRepository(AppDbContext dbContext) : IDepartmentRepository
+    {
+        private readonly AppDbContext _dbContext = dbContext;
+
+        public IEnumerable<Department> GetAll(bool WithAsNoTracking = true)
+        {
+            if (WithAsNoTracking)
+            {
+                return _dbContext.Departments.AsNoTracking().ToList();
+            }
+            return _dbContext.Departments.ToList();
+        }
+
+        public Department? GetById(int id)
+        {
+            return _dbContext.Departments.Find(id);
+        }
+
+        public int Add(Department entity)
+        {
+            _dbContext.Departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Corporation.DAL/Persistence/Repositories/Departments/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Corporation.DAL.Models.Department;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporation.DAL.Persistence.Repositories.Departments
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll(bool WithAsNoTracking = true);
+        Department? GetById(int id);
+        int Add(Department entity);
+        int Update(Department entity);
+        int Delete(Department entity);
+    }
+}
