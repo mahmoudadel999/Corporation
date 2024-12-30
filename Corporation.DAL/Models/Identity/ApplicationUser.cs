30a23f8c675681bd937254c470b469855dd28b46
Mamhoud Adel <mahmoudadel2199@gamil.com>
2024-12-30 16:58:41 +0200

    Identit package, Sign up, Sign in,, Sign out

---
+++ Corporation.DAL/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Corporation.DAL.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public bool IsAgree { get; set; }
+    }
+}
--- Corporation.DAL/Persistence/Data/AppDbContext.cs
+++ Corporation.DAL/Persistence/Data/AppDbContext.cs
@@ -1,11 +1,15 @@
 ﻿using Corporation.DAL.Models.Departments;
 using Corporation.DAL.Models.Employees;
+using Corporation.DAL.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Corporation.DAL.Persistence.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    public class AppDbContext(DbContextOptions<AppDbContext> options)
+        : IdentityDbContext<ApplicationUser>(options)
     {
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -15,6 +19,5 @@ namespace Corporation.DAL.Persistence.Data
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
-
     }
 }
